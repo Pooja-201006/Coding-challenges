7fdad3c0dd95b4715afcd10ae3b9d61c0002eb69
Pooja-201006 <poojapadmaramaraja@gmail.com>
2026-07-07 13:29:15 +0530

    Update 283_MoveZeroes.java

--- LeetCode/Arrays/283_MoveZeroes.java
+++ LeetCode/Arrays/283_MoveZeroes.java
@@ -1,1 +1,21 @@
 
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n=nums.length;
+        for(int i=0;i<n;i++)
+            {
+                if(nums[i]==0)
+                {
+                   int last=nums[i];
+                 for(int j=i;j<n-1;j++)
+                       {
+                                          nums[j]=nums[j+1];
+                       }
+                    nums[n-1]=last;
+                    i--;
+                    n--;
+                 }
+            
+            }
+    }
+}
