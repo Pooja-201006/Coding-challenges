c6a95a28756ddcc0da359752a3a636921db979f2
Pooja-201006 <poojapadmaramaraja@gmail.com>
2026-08-20 21:39:08 +0530

    Update firstlastsortelement.java

--- LeetCode/Arrays/firstlastsortelement.java
+++ LeetCode/Arrays/firstlastsortelement.java
@@ -16,3 +16,41 @@ class Solution {
         return new int[] {first,last};
     }
 }
+binary search method:
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int first=search(nums,target,true);
+      int last=search(nums,target,false);
+      return new int[]{first,last} ;
+   }
+public int search(int[] nums,int target,boolean isfirst)
+    {
+       int low=0;
+        int high=nums.length-1;
+        int ans=-1;
+        while(low<=high)
+            {
+             int mid=low+(high-low)/2;
+                if(nums[mid]==target)
+                {
+                    ans=mid;
+                    if(isfirst)
+                    {                                      high=mid-1;
+                     }
+                    else
+                        low=mid+1;
+
+                }
+                else if(nums[mid]>target)
+                {
+                    high=mid-1;
+                }
+                else
+                    low=mid+1;
+            }
+        
+       return ans;
+        
+}
+
+}
