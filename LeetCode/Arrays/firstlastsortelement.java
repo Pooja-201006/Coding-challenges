3e146afdfafa61a7eb1f0a88923c73a5dee629e3
Pooja-201006 <poojapadmaramaraja@gmail.com>
2026-08-20 21:21:55 +0530

    Create firstlastsortelement.java

---
+++ LeetCode/Arrays/firstlastsortelement.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int first=-1;
+        int last=-1;
+        for(int i=0;i<nums.length;i++)
+            {
+                if(target==nums[i])
+                {
+                     if(first==-1)
+                     {
+                         first=i;
+                     }
+                     last=i;
+                }
+            }
+        return new int[] {first,last};
+    }
+}
