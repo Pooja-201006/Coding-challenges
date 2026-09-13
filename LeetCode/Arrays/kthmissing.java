9753148d03a0cfbea1b3a9e672ea6ddc4da34773
Pooja-201006 <poojapadmaramaraja@gmail.com>
2026-09-13 11:21:20 +0530

    Create kthmissing.java

---
+++ LeetCode/Arrays/kthmissing.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        int num=1;
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]==num)
+            {
+                 num++;
+            }
+            else
+            {
+            k--;
+                if(k==0)
+                {
+                    return num;
+                }
+            i--;
+            num++;
+            } 
+        }
+        return num+k-1;
+    }
+}
